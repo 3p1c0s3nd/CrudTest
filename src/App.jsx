22f0620cb26a7741e0701e82c3d2401526f8ec80
3p1c0w3nd <3p1c0w3nd@1337.com>
2023-11-11 08:35:01 -0500

    Subimos el proyecto de un crud de usuarios para administrar desde un html

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import useCrud from "./hooks/useCrud";
+import FormUser from "./components/FormUser";
+import UserCard from "./components/UserCard";
+
+function App() {
+  const [isDisable, setIsDisable] = useState(false);
+  const [infoUpdate, setInfoUpdate] = useState();
+  const url = "https://users-crud.academlo.tech";
+  const [users, getUsers, createUsers, deleteUsers, updateUsers] = useCrud(url);
+  useEffect(() => {
+    getUsers("/users");
+  }, []);
+
+  const handleCreateUser = () => {
+    setInfoUpdate();
+    setIsDisable(false);
+  };
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold ">Users</h1>
+        <div className="flex justify-end">
+          <button onClick={handleCreateUser} className="form__btn_create">Create New User</button>
+        </div>
+        
+        <FormUser
+          createUsers={createUsers}
+          infoUpdate={infoUpdate}
+          updateUsers={updateUsers}
+          setInfoUpdate={setInfoUpdate}
+          isDisable={isDisable}
+          setIsDisable={setIsDisable}
+        />
+      </div>
+      <div className="flex flex-wrap">
+        {users?.map((user) => (
+          <UserCard
+            key={user.id}
+            user={user}
+            deleteUsers={deleteUsers}
+            setInfoUpdate={setInfoUpdate}
+            setIsDisable={setIsDisable}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
